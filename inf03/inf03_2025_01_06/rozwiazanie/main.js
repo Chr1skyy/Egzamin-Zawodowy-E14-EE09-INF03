8ce6e8364de46513b843c1879f7286f74fb90035
Chr1skyy <103789003+Chr1skyy@users.noreply.github.com>
2025-06-06 19:17:50 +0200

    fix

--- inf03/inf03_2025_01_06/rozwiazanie/main.js
+++ inf03/inf03_2025_01_06/rozwiazanie/main.js
@@ -2,15 +2,20 @@ function przelicz() {
     const wynik = document.getElementById("wynik");
     let liczbaDziesietna = +document.getElementById("liczbaDziesietna").value;
     let liczbaBinarna = "";
-    let j = 1;
     while (liczbaDziesietna != 0) {
         liczbaBinarna = (liczbaDziesietna % 2) + liczbaBinarna;
         liczbaDziesietna = Math.floor(liczbaDziesietna / 2);
-        if (j == 4) {
-            j = 1;
-            liczbaBinarna = " " + liczbaBinarna;
-        }
-        j++;
     }
-    wynik.innerHTML = liczbaBinarna + "<sub>2</sub>";
+	
+	let liczbaBinarna2 = "";
+    let j = 0;
+    for (let i = liczbaBinarna.length - 1; i >= 0; i--) {
+        liczbaBinarna2 = liczbaBinarna[i] + liczbaBinarna2;
+        j++;
+        if (j % 4 == 0 && i != 0) {
+            liczbaBinarna2 = " " + liczbaBinarna2;
+        }
+    }
+
+    wynik.innerHTML = liczbaBinarna2 + "<sub>2</sub>";
 }
