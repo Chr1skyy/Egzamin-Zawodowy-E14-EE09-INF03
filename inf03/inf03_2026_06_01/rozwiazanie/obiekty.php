e104b1551bf41dc1cadb825009b307b73242af98
Chr1skyy <tofucek@gmail.com>
2026-06-11 19:52:13 +0200

    Update obiekty.php

--- inf03/inf03_2026_06_01/rozwiazanie/obiekty.php
+++ inf03/inf03_2026_06_01/rozwiazanie/obiekty.php
@@ -8,58 +8,62 @@
 </head>
 
 <body>
-    <header>
-        <h1><a href="islandia.php">Zwiedzaj Islandię</a></h1>
-    </header>
-    <aside>
-        <h3>Do zwiedzania</h3>
-        <ul>
-            <li>Wodospady:
-                <ol>
+    <div id="container-row">
+        <div id="container-column">
+            <header>
+                <h1><a href="islandia.php">Zwiedzaj Islandię</a></h1>
+            </header>
+            <main>
+                <h2>Opis miejsca</h2>
+                <section>
                     <?php
                     $connect = mysqli_connect('localhost', 'root', '', 'islandia');
-                    $query3 = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idObiekt = 46";
-                    $result = mysqli_query($connect, $query3);
-                    while ($row = mysqli_fetch_array($result)) {
-                        echo "<li>{$row['nazwa']}</li>";
+                    if (isset($_GET['idObiekt'])) {
+                        $idObiekt = $_GET['idObiekt'];
+                        $query2modified = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idObiekt = $idObiekt;";
+                        $result = mysqli_query($connect, $query2modified);
+                        $row = mysqli_fetch_array($result);
+                        echo "<img src='{$row['plik']}' alt='{$row['nazwa']}'>";
+                        echo "<h2>{$row['nazwa']}</h2>";
+                        echo "<h3>{$row['rodzaj']}</h3>";
+                        echo "<p>{$row['nazwaCechy']}: {$row['wartoscCechy']}</p>";
+                        echo "<p>{$row['opis']}</p>";
                     }
+
+
                     ?>
+                </section>
+            </main>
+        </div>
+        <aside>
+            <h3>Do zwiedzania</h3>
+            <ul>
+                <li>Wodospady:
+                    <ol>
+                        <?php
+                        $query3 = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idObiekt = 46";
+                        $result = mysqli_query($connect, $query3);
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<li>{$row['nazwa']}</li>";
+                        }
+                        ?>
 
-                </ol>
-            </li>
-            <li>Siedliska zwierząt:
-                <ol>
-                    <?php
-                    $query3modified = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idRodzaj = 14";
-                    $result = mysqli_query($connect, $query3modified);
-                    while ($row = mysqli_fetch_array($result)) {
-                        echo "<li>{$row['nazwa']}</li>";
-                    }
-                    ?>
-                </ol>
-            </li>
-        </ul>
-    </aside>
-    <main>
-        <h2>Opis miejsca</h2>
-        <section>
-            <?php
-            if (isset($_GET['idObiekt'])) {
-                $idObiekt = $_GET['idObiekt'];
-                $query2modified = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idObiekt = $idObiekt;";
-                $result = mysqli_query($connect, $query2modified);
-                $row = mysqli_fetch_array($result);
-                echo "<img src='{$row['plik']}' alt='{$row['nazwa']}'>";
-                echo "<h2>{$row['nazwa']}</h2>";
-                echo "<h3>{$row['rodzaj']}</h3>";
-                echo "<p>{$row['nazwaCechy']}: {$row['wartoscCechy']}</p>";
-                echo "<p>{$row['opis']}</p>";
-            }
-
-
-            ?>
-        </section>
-    </main>
+                    </ol>
+                </li>
+                <li>Siedliska zwierząt:
+                    <ol>
+                        <?php
+                        $query3modified = "SELECT plik, nazwa, nazwaCechy, wartoscCechy, opis, rodzaj FROM obiekty JOIN rodzaje USING (idRodzaj) WHERE idRodzaj = 14";
+                        $result = mysqli_query($connect, $query3modified);
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<li>{$row['nazwa']}</li>";
+                        }
+                        ?>
+                    </ol>
+                </li>
+            </ul>
+        </aside>
+    </div>
     <footer>
         <hr>
         <p>Autor: Chr1skyy</p>
