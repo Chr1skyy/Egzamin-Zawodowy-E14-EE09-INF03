1a3b7b415ddf2b9bf5af30c72d68ed5ef634679b
Chr1skyy <tofucek@gmail.com>
2026-08-29 14:04:31 +0200

    Fix

--- ee09/ee09_2022_06_04/rozwiazanie/main.js
+++ ee09/ee09_2022_06_04/rozwiazanie/main.js
@@ -1,70 +1,69 @@
-/* Dla początkujących */
-function sprawdz2() {
-    const produkty = ['p1', 'p2', 'p3', 'p4']
+// Wersja 1
+let idZamowienia = 0;
+
+function sprawdz() {
+    const produkty = ['p1', 'p2', 'p3', 'p4'];
     for (let i = 0; i < produkty.length; i++) {
-        let ilosc = document.getElementById(produkty[i]).innerHTML
-        let produkt = document.getElementById(produkty[i])
-        if (ilosc == 0) {
-            produkt.style = 'background-color: red;'
-        }
-        if (ilosc >= 1 && ilosc <= 5) {
-            produkt.style = 'background-color: yellow;'
-        }
-        if (ilosc > 5) {
-            produkt.style = 'background-color: Honeydew;'
+        const produkt = document.getElementById(produkty[i]);
+        const ilosc = parseInt(produkt.innerText);
+
+        if (ilosc === 0) {
+            produkt.style.backgroundColor = 'red';
+        } else if (ilosc >= 1 && ilosc <= 5) {
+            produkt.style.backgroundColor = 'yellow';
+        } else if (ilosc > 5) {
+            produkt.style.backgroundColor = 'honeydew';
         }
     }
 }
 
 function aktualizuj(produktID) {
-    const produkt = document.getElementById(produktID)
-    const nowaIlosc = prompt('Podaj nową ilość:')
-    produkt.innerHTML = nowaIlosc
-    sprawdz2()
+    const nowaIlosc = prompt('Podaj nową ilość:');
+    const produkt = document.getElementById(produktID);
+    produkt.innerText = nowaIlosc;
+    sprawdz();
 }
 
-let idZamowienia = 0
 function zamow(nazwaProduktu) {
-    alert('Zamówienie nr: ' + idZamowienia + ' Produkt: ' + nazwaProduktu)
-    idZamowienia++
-}
-sprawdz2()
-//*/
-
-/* Dla kumatych *
-const iloscProduktow = document.querySelectorAll('.ilosc')
-const nazwaProduktow = document.querySelectorAll('.produkt')
-const aktualizujPrzycisk = document.querySelectorAll('.aktualizuj')
-const zamowPrzycisk = document.querySelectorAll('.zamow')
-let idZamowienia = 0
-
-function sprawdz() {
-    iloscProduktow.forEach((produkt) => {
-        if (produkt.innerHTML == 0) {
-            produkt.style = 'background-color: red;'
-        }
-        if (produkt.innerHTML >= 1 && produkt.innerHTML <= 5) {
-            produkt.style = 'background-color: yellow;'
-        }
-        if (produkt.innerHTML > 5) {
-            produkt.style = 'background-color: Honeydew;'
-        }
-    })
+    alert('Zamówienie nr: ' + idZamowienia + ' Produkt: ' + nazwaProduktu);
+    idZamowienia++;
 }
 
-aktualizujPrzycisk.forEach((przycisk, i) => {
-    przycisk.addEventListener('click', () => {
-        const nowaIlosc = prompt('Podaj nową ilość:')
-        iloscProduktow[i].innerHTML = nowaIlosc
-        sprawdz()
-    })
-})
+sprawdz();
 
-zamowPrzycisk.forEach((przycisk, i) => {
+// Wersja 2
+/*
+let idZamowienia = 0;
+
+function sprawdzZaawansowane() {
+    document.querySelectorAll('.ilosc').forEach((el) => {
+        const ilosc = parseInt(el.innerText);
+        if (ilosc === 0) {
+            el.style.backgroundColor = 'red';
+        } else if (ilosc >= 1 && ilosc <= 5) {
+            el.style.backgroundColor = 'yellow';
+        } else if (ilosc > 5) {
+            el.style.backgroundColor = 'honeydew';
+        }
+    });
+}
+
+document.querySelectorAll('.aktualizuj').forEach((przycisk) => {
     przycisk.addEventListener('click', () => {
-        alert('Zamówienie nr: ' + idZamowienia + ' Produkt: ' + nazwaProduktow[i].innerHTML)
-        idZamowienia++
-    })
-})
-sprawdz()
-//*/
+        const nowaIlosc = prompt('Podaj nową ilość:');
+        const komorkaIlosc = przycisk.closest('tr').querySelector('.ilosc');
+        komorkaIlosc.innerText = nowaIlosc;
+        sprawdzZaawansowane();
+    });
+});
+
+document.querySelectorAll('.zamow').forEach((przycisk) => {
+    przycisk.addEventListener('click', () => {
+        const nazwa = przycisk.closest('tr').querySelector('.produkt').innerText;
+        alert('Zamówienie nr: ' + idZamowienia + ' Produkt: ' + nazwa);
+        idZamowienia++;
+    });
+});
+
+sprawdzZaawansowane();
+*/
